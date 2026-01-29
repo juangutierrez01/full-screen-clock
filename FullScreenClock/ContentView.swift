6b63719a1e9bbfc9ffd96d4ffd8c6a4d269b8436
Juan Gutierrez <24966258+juangutie@users.noreply.github.com>
2026-01-29 01:23:54 -0500

    feat: add portrait clock

--- FullScreenClock/ContentView.swift
+++ FullScreenClock/ContentView.swift
@@ -2,13 +2,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TimelineView(.periodic(from: Date(), by: 0.01)) { context in
+            VStack {
+                Text(context.date.formatted(.dateTime.hour(.twoDigits(amPM: .omitted))))
+                Text(context.date.formatted(.dateTime.minute(.twoDigits)))
+                Text(context.date.formatted(.dateTime.second(.twoDigits)))
+            }
+            .font(.system(size: 200, design: .monospaced))
         }
-        .padding()
     }
 }
 
