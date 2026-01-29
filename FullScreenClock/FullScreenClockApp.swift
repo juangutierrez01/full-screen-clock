570937fa925c4c27d98012ef07bd313691b7ad5e
Juan Gutierrez <juan.gutierrez0157@gmail.com>
2026-01-28 23:48:28 -0500

    chore: add initial files

---
+++ FullScreenClock/FullScreenClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullScreenClockApp.swift
+//  FullScreenClock
+//
+//  Created by Juan Gutierrez on 2026-01-28.
+//
+
+import SwiftUI
+
+@main
+struct FullScreenClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
