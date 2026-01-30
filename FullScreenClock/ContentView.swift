2f934f2ae4009905bd16f6dbe216c7d63c395e4d
Juan Gutierrez <24966258+juangutie@users.noreply.github.com>
2026-01-29 22:06:19 -0500

    feat: smoother orientation transition
    Use [AnyLayout](https://developer.apple.com/documentation/swiftui/anylayout)
    "to enable dynamically changing the type of a layout container
    without destroying the state of the subviews"

--- FullScreenClock/ContentView.swift
+++ FullScreenClock/ContentView.swift
@@ -4,27 +4,23 @@ struct ContentView: View {
     @Environment(\.horizontalSizeClass) private var horizontalSizeClass
 
     var body: some View {
-        if horizontalSizeClass == .regular {
-            TimelineView(.periodic(from: Date(), by: 0.01)) { context in
-                HStack {
-                    Text(context.date.formatted(.dateTime.hour(.twoDigits(amPM: .omitted))))
+        let isHorizontal = horizontalSizeClass == .regular
+        let layout = isHorizontal ? AnyLayout(HStackLayout()) : AnyLayout(VStackLayout())
+        let digitSize = isHorizontal ? 190.0 : 230.0
+
+        TimelineView(.periodic(from: Date(), by: 0.01)) { context in
+            layout {
+                Text(context.date.formatted(.dateTime.hour(.twoDigits(amPM: .omitted))))
+                if isHorizontal {
                     Text(":").font(.system(size: 120))
-                    Text(context.date.formatted(.dateTime.minute(.twoDigits)))
+                }
+                Text(context.date.formatted(.dateTime.minute(.twoDigits)))
+                if isHorizontal {
                     Text(":").font(.system(size: 120))
-                    Text(context.date.formatted(.dateTime.second(.twoDigits)))
                 }
-                .font(.system(size: 190, weight: .bold, design: .monospaced))
+                Text(context.date.formatted(.dateTime.second(.twoDigits)))
             }
-        }
-        else {
-            TimelineView(.periodic(from: Date(), by: 0.01)) { context in
-                VStack {
-                    Text(context.date.formatted(.dateTime.hour(.twoDigits(amPM: .omitted))))
-                    Text(context.date.formatted(.dateTime.minute(.twoDigits)))
-                    Text(context.date.formatted(.dateTime.second(.twoDigits)))
-                }
-                .font(.system(size: 230, weight: .bold, design: .monospaced))
-            }
+            .font(.system(size: digitSize, weight: .bold, design: .monospaced))
         }
     }
 }
