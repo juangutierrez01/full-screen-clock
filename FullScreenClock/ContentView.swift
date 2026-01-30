f131bccad0ea031e643229b4518bbb6b2a4d9c4f
Juan Gutierrez <juan.gutierrez0157@gmail.com>
2026-01-29 21:41:43 -0500

    feat: add landscape clock

--- FullScreenClock/ContentView.swift
+++ FullScreenClock/ContentView.swift
@@ -1,14 +1,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+
     var body: some View {
-        TimelineView(.periodic(from: Date(), by: 0.01)) { context in
-            VStack {
-                Text(context.date.formatted(.dateTime.hour(.twoDigits(amPM: .omitted))))
-                Text(context.date.formatted(.dateTime.minute(.twoDigits)))
-                Text(context.date.formatted(.dateTime.second(.twoDigits)))
+        if horizontalSizeClass == .regular {
+            TimelineView(.periodic(from: Date(), by: 0.01)) { context in
+                HStack {
+                    Text(context.date.formatted(.dateTime.hour(.twoDigits(amPM: .omitted))))
+                    Text(":").font(.system(size: 120))
+                    Text(context.date.formatted(.dateTime.minute(.twoDigits)))
+                    Text(":").font(.system(size: 120))
+                    Text(context.date.formatted(.dateTime.second(.twoDigits)))
+                }
+                .font(.system(size: 190, weight: .bold, design: .monospaced))
             }
-            .font(.system(size: 210, weight: .bold, design: .monospaced))
+        }
+        else {
+            TimelineView(.periodic(from: Date(), by: 0.01)) { context in
+                VStack {
+                    Text(context.date.formatted(.dateTime.hour(.twoDigits(amPM: .omitted))))
+                    Text(context.date.formatted(.dateTime.minute(.twoDigits)))
+                    Text(context.date.formatted(.dateTime.second(.twoDigits)))
+                }
+                .font(.system(size: 230, weight: .bold, design: .monospaced))
+            }
         }
     }
 }
