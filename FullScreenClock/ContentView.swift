e400f0975d0c504779baba1fc154bce2df68ac6c
Juan Gutierrez <juan.gutierrez0157@gmail.com>
2026-02-05 16:47:48 -0500

    fix: monospace and colon size
    Make all digits monospace and make colon separator larger.

--- FullScreenClock/ContentView.swift
+++ FullScreenClock/ContentView.swift
@@ -8,7 +8,7 @@ struct ContentView: View {
         let layout = isLandscape
             ? AnyLayout(HStackLayout(alignment: .firstTextBaseline))
             : AnyLayout(VStackLayout(spacing: -40))
-        let defaultSize = 250.0
+        let defaultSize = 240.0
         let defaultWeight = Font.Weight.regular
         let defaultDesign = Font.Design.rounded
 
@@ -17,16 +17,15 @@ struct ContentView: View {
                 Text(context.date.formatted(.dateTime.hour(.twoDigits(amPM: .omitted))))
                 if isLandscape {
                     Text(":")
-                        .font(.system(size: 120))
-                        .offset(y: -40)
-                        .padding(.horizontal, -10)
+                        .offset(y: -20)
+                        .padding(.horizontal, -20)
                 }
                 Text(context.date.formatted(.dateTime.minute(.twoDigits)))
                 Text(context.date.formatted(.dateTime.second(.twoDigits)))
                     .font(.system(size: isLandscape ? 140 : defaultSize, weight: defaultWeight, design: defaultDesign))
-                    .monospacedDigit()
             }
             .font(.system(size: defaultSize, weight: defaultWeight, design: defaultDesign))
+            .monospacedDigit()
         }
     }
 }
